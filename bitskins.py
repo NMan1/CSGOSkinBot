eb62c9dc82412d70d5dee17432f21d3f9e84e047
NMan1 <nickmantini01@gmail.com>
2020-08-29 20:48:44 -0500

    First commit

---
+++ bitskins.py
@@ -0,0 +1,22 @@
+import time
+import requests
+import pyotp
+import base64
+
+
+class Bitskins:
+    def __init__(self, apikey, secretkey):
+        self.apikey = apikey
+        self.secretkey = secretkey
+
+    def get_request(self, url, params_vars=None):
+        auth_token = pyotp.TOTP(self.secretkey)
+        params_req = {"api_key": self.apikey, "code": auth_token.now()}
+
+        if params_vars:
+            params = {**params_req, **params_vars}
+        else:
+            params = params_req
+
+        r = requests.get("https://bitskins.com/api/v1/" + url, params=params)
+        return r.json()
