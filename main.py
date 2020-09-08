c56e5c3cbcb3c2ff7d654e997044323878bc7e50
NMan1 <nickmantini01@gmail.com>
2020-09-08 10:17:39 -0500

    Changes

--- main.py
+++ main.py
@@ -28,6 +28,63 @@ async def track(ctx, weapon, *, arg=None):
 
 
 @client.command(pass_context=True)
+async def deal(ctx):
+    market = bitskins.get_request("get_price_data_for_items_on_sale", {"app_id": 730})['data']['items']
+    most_discounted_item = None
+    last_discount = 0
+
+    for item in market:
+        try:
+            discount = int(
+                100 * (float(item['recent_sales_info']['average_price']) - float(item['lowest_price'])) / float(
+                    item['recent_sales_info']['average_price']))
+            if discount > last_discount:
+                cheapest_listing = bitskins.get_request("get_inventory_on_sale", {"market_hash_name": most_discounted_item['market_hash_name'], "max_price": most_discounted_item['lowest_price']})
+                cheapest_listing = cheapest_listing['data']['items'][0]
+                if cheapest_listing:
+                    last_discount = discount
+                    most_discounted_item = item
+                    print(discount)
+        except Exception:
+            pass
+
+    print(cheapest_listing)
+    cheapest_listing = cheapest_listing['data']['items'][0]
+    skin_img = cheapest_listing['image']
+
+    withdrawable_at = subtract_time(current_time(), from_timestamp(float(cheapest_listing['withdrawable_at'])))
+    discount = int(100 * (float(cheapest_listing['suggested_price']) - float(cheapest_listing['price'])) / float(cheapest_listing['suggested_price']))
+
+    discount = f"(%{discount} Off)" if 0 < discount <= 100 else ""
+
+    if withdrawable_at.days >= 0:
+        time = f"{withdrawable_at.days} Days, {withdrawable_at.seconds // 3600} hours"
+    else:
+        time = f"{withdrawable_at.days} Day, {withdrawable_at.seconds // 3600} hours"
+
+    if withdrawable_at.days > 1:
+        withdrable_text = f"""```css\n[{time}]```"""
+    elif 1 >= withdrawable_at.days >= 0:
+        withdrable_text = f"""```css\n{time}```"""
+    else:
+        withdrable_text = f"""```css\nInstantly Withdrawable!```"""
+
+    color = 0xcaab05
+    for rarity in raritys:
+        if rarity[0] == cheapest_listing['tags']['rarity']:
+            color = rarity[1]
+
+    embed = skin_embed(weapon, skin, quality, color, skin_img, withdrable_text, data, cheapest_listing, discount)
+
+    if data['recent_sales_info']:
+        embed.add_field(name=f"Average Price (over {int(float(data['recent_sales_info']['hours']))} hours)", value=f"${data['recent_sales_info']['average_price']}", inline=False)
+    else:
+        embed.add_field(name=f"Average Price", value=f"Failed to find average price - no recent sales", inline=False)
+    await ctx.send(embed=embed)
+
+
+
+@client.command(pass_context=True)
 async def price(ctx, weapon, *, arg=None):
     weapon, skin, quality = make_weapon_choice(weapon, arg)
     weapon = clean_weapon(weapon)
